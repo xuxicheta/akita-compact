e3af5380543c1eb0906a167807571ec19c4237b3
Anton Shvets <antshvets@gmail.com>
2020-03-10 17:05:55 +0300

    separate helpers folder

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { Rule, SchematicContext, Tree, url, apply, template, mergeWith, move } from '@angular-devkit/schematics';
 import { strings, normalize } from '@angular-devkit/core';
-import { getDefaultPath } from '../get-default-path';
+import { getDefaultPath } from '../_helpers/get-default-path.helper';
+
 
 interface Schema {
   name: string;
